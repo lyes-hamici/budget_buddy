af6ae67ac8055d1a80ef07f02380b38b1353197f
lyes-hamici <lyes.hamici@laplateforme.io>
2024-03-26 10:02:27 +0100

    <feat>[Window.py]:<Creation of the graphical window>

--- view/Window.py
+++ view/Window.py
@@ -1,12 +1,18 @@
-import tkinter as tk
-class Window(tk.Tk):
+import customtkinter as ctk
+class Window(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title("Tkinter MVC")
         self.geometry("1280x720")
         self.resizable(False, False)
+        self.geometry("1280x720")
+        self.config(bg="cornsilk1")
+
+    
+    def main(self):
+        self.mainloop()
    
     
 if __name__ == "__main__":
     app = Window()
-    app.mainloop()
+    app.main()
