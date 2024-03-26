9c474f53ab5aa360c3a4bcc92ca3e2c8a5f066c1
lyes-hamici <lyes.hamici@laplateforme.io>
2024-03-26 13:34:36 +0100

    <feat>[Window.py]:<Add a function to call the home page>

--- view/Window.py
+++ view/Window.py
@@ -1,18 +1,24 @@
 import customtkinter as ctk
+from Home import Home
+
 class Window(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title("Tkinter MVC")
         self.geometry("1280x720")
         self.resizable(False, False)
-        self.geometry("1280x720")
         self.config(bg="cornsilk1")
 
     
     def main(self):
         self.mainloop()
+
+    def display_home_page(self):
+        self.home = Home(self)
+        self.home.pack(expand=True, fill='both')
+        self.main()
    
     
 if __name__ == "__main__":
     app = Window()
-    app.main()
+    app.display_home_page()
