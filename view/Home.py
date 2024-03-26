160c80fa1675d2539858c16b7adcaa29c8a87dc4
lyes-hamici <lyes.hamici@laplateforme.io>
2024-03-26 13:41:00 +0100

    <feat>[Home.py]:<Add a setter and getter for self.balance>

--- view/Home.py
+++ view/Home.py
@@ -5,7 +5,7 @@ class Home(CTkFrame):
     def __init__(self,master):
         super().__init__(master)
         self.running = True
-        self.sold = 0
+        self.balance = 0
         self.create_widgets()
 
 
@@ -17,11 +17,25 @@ class Home(CTkFrame):
         frame2 = CTkFrame(master=self,fg_color="azure3", border_width=0, width=7000)
         frame2.pack(expand=False, side=ctk.TOP,pady = 50)
 
-        label_sold = ctk.CTkLabel(master=frame, text=f'Balance : {self.sold} €', font=('helvetica', 64))#Replace self.sold with a the output of a function for get the sold of the account
+        label_sold = ctk.CTkLabel(master=frame, text=f'Balance : {self.balance} €', font=('helvetica', 64))#Replace self.balance with a the output of a function for get the sold of the account
         label_sold.pack(pady=12, padx=100)
 
-        label =  ctk.CTkLabel(master=frame2, text='last transaction', font=('helvetica', 30))#Replace self.sold with a the output of a function for get the sold of the account
+        label =  ctk.CTkLabel(master=frame2, text='last transaction', font=('helvetica', 30))#Replace text with a the output of a function for get the 3 last transaction
         label.pack(pady=12, padx=100)
+
+
+
+
+    def set_balance(self,new_balance):
+        self.balance = new_balance 
+
+
+    def get_balance(self):
+        print(self.balance)
+        return self.balance
+
+
+
 
 
 
