b23bcf0abd83b353a97150ec5beac2648fa91cc8
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-03-27 23:04:21 +0100

    <creat>[window]:<adding setters and getters for the variables>

--- view/Window.py
+++ view/Window.py
@@ -1,9 +1,9 @@
 import customtkinter as ctk
 from .LoginFrame import LoginFrame
 from .RegisterFrame import RegisterFrame
-from Home import Home
-from Account import Account
-from Dashboard import Dashboard
+from .Home import Home
+from .Account import Account
+from .Dashboard import Dashboard
 
 class Window(ctk.CTk):
     '''
@@ -22,13 +22,67 @@ class Window(ctk.CTk):
         self.value_name = ctk.StringVar()
         self.value_password = ctk.StringVar()
         self.value_remember_me = ctk.BooleanVar()
+        self.balance = ctk.DoubleVar()
         self.value_display_page = 1
+
+    #=================GETTERS & SETTERS=======================#
+    #=================GETTERS=======================#
+    def get_value_name(self):
+        '''
+        This method returns the value of the name.
+        '''
+        return self.value_name
+    
+    def get_value_password(self):
+        '''
+        This method returns the value of the password.
+        '''
+        return self.value_password
+    
+    def get_value_remember_me(self):
+        '''
+        This method returns the value of the remember me.
+        '''
+        return self.value_remember_me
+    
+    def get_balance(self):
+        '''
+        This method returns the value of the balance.
+        '''
+        return self.balance
+    
+    #=================SETTERS=======================#
+    def set_value_name(self, value):
+        '''
+        This method sets the value of the name.
+        '''
+        self.value_name = value
+
+    def set_value_password(self, value):
+        '''
+        This method sets the value of the password.
+        '''
+        self.value_password = value
+
+    def set_value_remember_me(self, value):
+        '''
+        This method sets the value of the remember me.
+        '''
+        self.value_remember_me = value
+    
+    def set_balance(self, value):
+        '''
+        This method sets the value of the balance.
+        '''
+        self.balance = value
 
     def set_value_display_page(self, value):
         '''
         This method sets the value of the display page.
         '''
         self.value_display_page = value
+    
+    #=================DISPLAY METHODS=======================#
 
     def displayLoginPage(self):
         '''
@@ -49,7 +103,6 @@ class Window(ctk.CTk):
         this method displays the register page.
         '''
         self.register_frame = RegisterFrame(self)
-        self.forgetLoginPage()
         self.register_frame.pack(fill='both', expand=True)
     
     def forgetRegisterPage(self):
@@ -59,26 +112,35 @@ class Window(ctk.CTk):
         self.register_frame.pack_forget()
         self.config(bg="cornsilk1")
 
-    
-    def main(self):
-        self.mainloop()
-
     def display_home_page(self):
+        '''
+        This method displays the home page.
+        '''
         self.home = Home(self)
-        self.home.pack(expand=True, fill='both')
-        self.main()
+        self.display_dashboard()
+        self.home.pack(side='left', expand=True, fill='both')
 
 
     def display_account_page(self):
+        '''
+        This method displays the account page.
+        '''
+        self.display_dashboard()
         self.account = Account(self)
-        self.account.pack(expand=True, fill='both')
-        self.main()
+        self.account.pack(side='left', expand=True, fill='both')
     
     def display_dashboard(self):
+        '''
+        This method displays the dashboard.
+        '''
         self.dashboard = Dashboard(self)
-        self.dashboard.pack(expand= True, fill='both')
-        self.main()
+        self.dashboard.pack(side='left', expand= True, fill='both')
 
+    def main(self):
+        '''
+        This method is the main loop of the application.
+        '''
+        self.mainloop()
     
 if __name__ == "__main__":
     app = Window()
