d5ecad1f017152ff9643423bbf2162ad581809a2
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-03-27 16:22:55 +0100

    <feat>[controller]:<adding a method called observer that is threaded and look constantly for event in the view we also created the method change display that allow the controller to set different frame on the window that is called in the observer method>

--- controller.py
+++ controller.py
@@ -2,12 +2,35 @@ from view import Window
 from model import Transaction_repository, User_repository
 from services import Db
 
+import threading
+import time
+
 class Controller:
     def __init__(self):
         self.db = Db()
         self.Transaction_repository = Transaction_repository(self.db)
         self.User_repository = User_repository()
         self.view = Window()
+        self.thread = threading.Thread(target=self.observer)
+        self.thread.start()
+
+    def observer(self):
+        while True:
+            if self.view.value_display_page != 0:
+                self.change_display()
+            time.sleep(0.1)
+
+    
+   
+    
+    def change_display(self):
+            if self.view.value_display_page == 1:
+                self.view.displayLoginPage()
+                self.view.set_value_display_page(0)
+            if self.view.value_display_page == 2:
+                self.view.displayRegisterPage()
+                self.view.set_value_display_page(0)
+
 
     def main(self):
         self.view.mainloop()
