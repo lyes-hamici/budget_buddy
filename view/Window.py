8fbb1ff6ba6f5eb87acc9822f390f7455de44e01
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-03-27 16:29:40 +0100

    <create>[value_name][value_password][value_remember_me][display_page]:<variables that will stock user input from login page>

--- view/Window.py
+++ view/Window.py
@@ -1,11 +1,58 @@
-import tkinter as tk
-class Window(tk.Tk):
+import customtkinter as ctk
+from .LoginFrame import LoginFrame
+from .RegisterFrame import RegisterFrame
+
+class Window(ctk.CTk):
+    '''
+    This class is the main window of the application. It is the parent of all the other frames. It is the main view of the application.
+    '''
     def __init__(self):
+        '''
+        Constructor of the class. It initializes the main window of the application.
+        '''
         super().__init__()
         self.title("Tkinter MVC")
         self.geometry("1280x720")
         self.resizable(False, False)
-   
+        self.value_name = ctk.StringVar()
+        self.value_password = ctk.StringVar()
+        self.value_remember_me = ctk.BooleanVar()
+        self.value_display_page = 1
+
+    def set_value_display_page(self, value):
+        '''
+        This method sets the value of the display page.
+        '''
+        self.value_display_page = value
+
+    def displayLoginPage(self):
+        '''
+        this method displays the login page.
+        '''
+        self.login_frame = LoginFrame(self)
+        self.login_frame.pack(fill='both', expand=True)
+
+    def forgetLoginPage(self):
+        '''
+        This method hides the login page.
+        '''
+        self.login_frame.pack_forget()
+
+
+    def displayRegisterPage(self):
+        '''
+        this method displays the register page.
+        '''
+        self.register_frame = RegisterFrame(self)
+        self.forgetLoginPage()
+        self.register_frame.pack(fill='both', expand=True)
+    
+    def forgetRegisterPage(self):
+        '''
+        this method hides the register page.
+        '''
+        self.register_frame.pack_forget()
+
     
 if __name__ == "__main__":
     app = Window()
