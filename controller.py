889038055b38a9034703a139378c9230db446214
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-03-27 23:09:37 +0100

    <refact>[controller]:<Refactored Controller class to manage page displays and transitions>

--- controller.py
+++ controller.py
@@ -11,12 +11,18 @@ class Controller:
         self.Transaction_repository = Transaction_repository(self.db)
         self.User_repository = User_repository()
         self.view = Window()
+        self.old_value_display_page = 0
         self.thread = threading.Thread(target=self.observer)
         self.thread.start()
+
+    def set_old_value_display_page(self, value):
+        self.old_value_display_page = value
+        
 
     def observer(self):
         while True:
             if self.view.value_display_page != 0:
+                self.forget_display()
                 self.change_display()
             time.sleep(0.1)
 
@@ -26,10 +32,35 @@ class Controller:
     def change_display(self):
             if self.view.value_display_page == 1:
                 self.view.displayLoginPage()
+                self.set_old_value_display_page(1)
                 self.view.set_value_display_page(0)
             if self.view.value_display_page == 2:
                 self.view.displayRegisterPage()
+                self.set_old_value_display_page(2)
                 self.view.set_value_display_page(0)
+            if self.view.value_display_page == 3:
+                self.view.display_home_page()
+                self.set_old_value_display_page(3)
+                self.view.set_value_display_page(0)
+            if self.view.value_display_page == 4:
+                self.view.display_account_page()
+                self.set_old_value_display_page(4)
+                self.view.set_value_display_page(0)
+
+    def forget_display(self):
+        if self.old_value_display_page == 1:
+            self.view.forgetLoginPage()
+        elif self.old_value_display_page == 2:
+            self.view.forgetRegisterPage()
+        elif self.old_value_display_page == 3:
+            self.view.home.pack_forget()
+            self.view.dashboard.pack_forget()
+        elif self.old_value_display_page == 4:
+            self.view.account.pack_forget()
+            self.view.dashboard.pack_forget()
+        elif self.old_value_display_page == 5:
+            self.view.graphics.pack_forget()
+
 
 
     def main(self):
