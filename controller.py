549a45247eb9848825f83b5330e6aef1e9721ade
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2024-03-27 16:02:06 +0100

    <feat>[Controller.py]:<added a thread to manipulate a loop in wich the controller will check for differents states of view>

--- controller.py
+++ controller.py
@@ -1,3 +1,4 @@
+import threading
 from view import Window
 from model import Transaction_repository, User_repository
 from services import Db
@@ -10,4 +11,13 @@ class Controller:
         self.view = Window()
 
     def main(self):
+        controller_thread = threading.Thread(target=self.controller_loop)
+        controller_thread.start()
         self.view.mainloop()
+    
+    def controller_loop(self):
+        pass
+        
+if __name__ == "__main__":
+    controller = Controller()
+    controller.main()
