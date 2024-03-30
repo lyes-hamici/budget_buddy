4f7e87a5dc766822576a01b3171e44bcee952a16
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2024-03-30 10:40:41 +0100

    <feat>[Graph.py]:<added get_30_days_balance_list it return a list with the balances for each day in the past 30 days>

--- services/Graph.py
+++ services/Graph.py
@@ -1,4 +1,18 @@
+from datetime import datetime, timedelta
 
 class Graph:
-    def __init__(self) -> None:
+    def __init__(self,user, transaction_repository) -> None:
+        self.user = user
+        self.transaction_repository = transaction_repository
+    
+    def draw_graph(self, balance_list, date_list):
         pass
+    
+    def get_30_days_balance_list(self):
+        balance_list = []
+        for i in range(30):
+            date = datetime.now() - timedelta(days=i)
+            balance = self.transaction_repository.calculate_balance_by_date(self.user, date)
+            balance_list.append(balance)
+        return balance_list
+    
