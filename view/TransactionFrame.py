c9c73af93612f318b559a6d33fb1e8c87977da2d
lyes-hamici <lyes.hamici@laplateforme.io>
2024-03-30 16:29:16 +0100

    <feat>[TrasactionFrame.py]:<Add a template button remove and modify , we need to create a function to use this button.>

--- view/TransactionFrame.py
+++ view/TransactionFrame.py
@@ -19,36 +19,36 @@ class TransactionFrame():
             frame.after(200 * i, self.display_frame,frame, new_list)
             
     def display_frame(self, frame, i):
-        print("test display frame",i)
-        '''
-        Function that displays a transaction frame.
-
-        Args:
-            frame: The parent frame where the transaction frame will be displayed.
-            i: Index of the transaction frame.
-        '''
+        print("test display frame", i)
         frame_date = CTkFrame(master=frame, fg_color="transparent", bg_color="transparent")
         frame_date.pack(pady=15, fill=ctk.X)
 
         date_text = f"date {i[7]}"
-        date_label = CTkLabel(master=frame_date, text=date_text, font=('helvetica', 20))
+        date_label = CTkLabel(master=frame_date, text=date_text, font=('helvetica', 20),text_color='black')
         date_label.pack(side="top", pady=5, anchor=ctk.W, padx=5)
 
-        frame_transaction = CTkFrame(master=frame_date, border_color="black", border_width=2)  # Creating the frame
-        frame_transaction.pack(pady=15, fill=ctk.X, padx=5)  # Placing the frame within the main frame
+        frame_transaction = CTkFrame(master=frame_date, border_color="black", border_width=2)
+        frame_transaction.pack(pady=15, fill=ctk.X, padx=5)
 
-        label_text_nom = f"Name {i[2]} -"  # Label text
+        # Ajout des boutons
+        button1 = ctk.CTkButton(master=frame_transaction, text="Remove")
+        button1.pack(side="right", anchor="ne", padx=5, pady=5)
+
+        button2 = ctk.CTkButton(master=frame_transaction, text="Modify")
+        button2.pack(side="right", anchor="ne", padx=5, pady=5)
+
+        label_text_nom = f"Name {i[2]} -"
         label_nom = CTkLabel(master=frame_transaction, text=label_text_nom, font=('helvetica', 14))
-        label_nom.pack(pady=5,side="top",padx = 5,anchor="nw")  # Placing the label within the frame
+        label_nom.pack(pady=5, side="top", padx=5, anchor="nw")
 
-        label_text_description = f"Description : {i[3]}"  # Label text
+        label_text_description = f"Description : {i[3]}"
         label_description = CTkLabel(master=frame_transaction, text=label_text_description, font=('helvetica', 14))
-        label_description.pack(pady=5,side="top",padx = 5,anchor="n")  # Placing the label within the frame
+        label_description.pack(pady=5, side="top", padx=5, anchor="n")
 
-        label_text_category = f"Category : {i[5]}"  # Label text
+        label_text_category = f"Category : {i[5]}"
         label_category = CTkLabel(master=frame_transaction, text=label_text_category, font=('helvetica', 14))
-        label_category.pack(side="left", padx=5, pady=5, anchor="sw")  # Placing the label within the frame
+        label_category.pack(side="left", padx=5, pady=5, anchor="sw")
 
-        label_text_values = f"Values : {i[4]} €"  # Label text
+        label_text_values = f"Values : {i[4]} €"
         label_values = CTkLabel(master=frame_transaction, text=label_text_values, font=('helvetica', 14))
-        label_values.pack(side="right", padx=5, pady=5, anchor="se")  # Placing the label within the frame
+        label_values.pack(side="right", padx=5, pady=5, anchor="se")
