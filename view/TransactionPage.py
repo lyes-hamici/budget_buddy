ef4f5d2b7cfca0c8a148106da8ecb7c0cdd61448
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-04-01 13:26:45 +0200

    <refactor>:<refactor entry widgets and add transaction button in TransactionPage.py>

--- view/TransactionPage.py
+++ view/TransactionPage.py
@@ -20,45 +20,54 @@ class TrasactionPage(CTkFrame):
         frame2.pack(expand=True, side=ctk.TOP, fill='both')
 
         # Left side - Entry widgets
-        entry1 = CTkEntry(master=frame2, fg_color="white", width=20,placeholder_text="name")
-        entry2 = CTkEntry(master=frame2, fg_color="white", width=20,placeholder_text="Description")
-        entry3 = CTkEntry(master=frame2, fg_color="white", width=20,placeholder_text="Category")
-        entry4 = CTkEntry(master=frame2, fg_color="white", width=20,placeholder_text="Values")
-        entry5 = CTkEntry(master=frame2, fg_color="white", width=20,placeholder_text="Date")
+        entry_name = CTkEntry(master=frame2, fg_color="white", width=20,placeholder_text="name")
+        entry_description = CTkEntry(master=frame2, fg_color="white", width=20,placeholder_text="Description")
+        entry_category = CTkEntry(master=frame2, fg_color="white", width=20,placeholder_text="Category")
+        entry_value = CTkEntry(master=frame2, fg_color="white", width=20,placeholder_text="Values")
+        entry_date = CTkEntry(master=frame2, fg_color="white", width=20,placeholder_text="Date")
         
 
         # Right side - Button widgets
-        button1 = CTkButton(master=frame2, text="Add Transaction")
+        button1 = CTkButton(master=frame2, text="Add Transaction", command=self.on_add_transaction_button_click)
 
         button1.pack(pady=10, padx=50)
 
-        entry1.pack(pady=10, padx=10,fill = ctk.X)
-        entry2.pack(pady=10, padx=10,fill = ctk.X)
-        entry3.pack(pady=10, padx=10,fill = ctk.X)
-        entry4.pack(pady=10, padx=10,fill = ctk.X)
-        entry5.pack(pady=10, padx=10,fill = ctk.X)
+        entry_name.pack(pady=10, padx=10,fill = ctk.X)
+        entry_description.pack(pady=10, padx=10,fill = ctk.X)
+        entry_category.pack(pady=10, padx=10,fill = ctk.X)
+        entry_value.pack(pady=10, padx=10,fill = ctk.X)
+        entry_date.pack(pady=10, padx=10,fill = ctk.X)
 
         # Getter methods for entry widgets
-        self.entry1 = entry1
-        self.entry2 = entry2
-        self.entry3 = entry3
-        self.entry4 = entry4
-        self.entry5 = entry5
+        self.entry_name = entry_name
+        self.entry_description = entry_description
+        self.entry_category = entry_category
+        self.entry_value = entry_value
+        self.entry_date = entry_date
    
 
     # Getter methods to retrieve the content of each entry
-    def get_entry1_text(self):
-        return self.entry1.get()
+    def get_entry_name_text(self):
+        return self.entry_name.get()
 
-    def get_entry2_text(self):
-        return self.entry2.get()
+    def get_entry_description_text(self):
+        return self.entry_description.get()
 
-    def get_entry3_text(self):
-        return self.entry3.get()
+    def get_entry_category_text(self):
+        return self.entry_category.get()
     
+    def get_entry_value_text(self):
+        return self.entry_value.get()
 
-    def get_entry4_text(self):
-        return self.entry4.get()
+    def get_entry_date_text(self):
+        return self.entry_date.get()
 
-    def get_entry5_text(self):
-        return self.entry5.get()
+    def on_add_transaction_button_click(self):
+        self.master.set_add_transaction(True)
+        """
+        self.entry_name.delete(0, ctk.END)
+        self.entry_description.delete(0, ctk.END)
+        self.entry_category.delete(0, ctk.END)
+        self.entry_value.delete(0, ctk.END)
+        self.entry_date.delete(0, ctk.END)
+        """
